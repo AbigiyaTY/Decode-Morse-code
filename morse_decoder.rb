83958f1d0fd10ba6f90114b7f7904870a60200b6
Farida-Hamid <faridaaddala@gmail.com>
2022-11-09 18:13:41 +0300

    Change double quotes to single quotes

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -1,52 +1,52 @@
 def char_decoder(string)
   morse_dectionary = {
-    ".-" => "A",
-    "-..." => "B",
-    "-.-." => "C",
-    "-.." => "D",
-    "." => "E",
-    "..-." => "F",
-    "--." => "G",
-    "...." => "H",
-    ".." => "I",
-    ".---" => "J",
-    "-.-" => "K",
-    ".-.." => "L",
-    "--" => "M",
-    "-." => "N",
-    "---" => "O",
-    ".--." => "P",
-    "--.-" => "Q",
-    ".-." => "R",
-    "..." => "S",
-    "-" => "T",
-    "..-" => "U",
-    "...-" => "V",
-    ".--" => "W",
-    "-..-" => "X",
-    "-.--" => "Y",
-    "--.." => "Z",
+    '.-' => 'A',
+    '-...' => 'B',
+    '-.-.' => 'C',
+    '-..' => 'D',
+    '.' => 'E',
+    '..-.' => 'F',
+    '--.' => 'G',
+    '....' => 'H',
+    '..' => 'I',
+    '.---' => 'J',
+    '-.-' => 'K',
+    '.-..' => 'L',
+    '--' => 'M',
+    '-.' => 'N',
+    '---' => 'O',
+    '.--.' => 'P',
+    '--.-' => 'Q',
+    '.-.' => 'R',
+    '...' => 'S',
+    '-' => 'T',
+    '..-' => 'U',
+    '...-' => 'V',
+    '.--' => 'W',
+    '-..-' => 'X',
+    '-.--' => 'Y',
+    '--..' => 'Z',
   }
   morse_dectionary[string]
 end
 
-puts (char_decoder(".-"))
+puts (char_decoder('.-'))
 
 
 def word_decoder(string)
 
     split_letter = string.split
      
-    str = ""
+    str = ''
     split_letter.each { |letter| str += char_decoder(letter) }
     str
     
 end
 
-puts(word_decoder("-- -.--"))
+puts(word_decoder('-- -.--'))
 
 def sentence_decoder(string)
-  string.split("   ").map { |word| word_decoder(word) }.join(" ")
+  string.split('   ').map { |word| word_decoder(word) }.join(' ')
 end
 
-puts(sentence_decoder(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."))
+puts(sentence_decoder('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'))
